64c1d9aa0b5f9182a2961c50f84e75442e44b3dc
Yakov Borevich <j.borevich@gmail.com>
2022-11-22 11:33:09 +0100

    [widget.input] WIP

--- bevy_elements_widgets/src/input/mod.rs
+++ bevy_elements_widgets/src/input/mod.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, ui::FocusPolicy};
+use bevy::{prelude::*, ui::FocusPolicy, text::TextLayoutInfo};
 use bevy_elements_core::{*, element::{Element, DisplayElement}, builders::DefaultFont, attributes::AttributeValue};
 use bevy_elements_macro::*;
 
@@ -7,35 +7,43 @@ pub struct InputPlugins;
 impl Plugin for InputPlugins {
     fn build(&self, app: &mut App) {
         app.register_element_builder("text", build_text);
-        app.register_element_builder("text-input", build_text_input);
+        app.add_system(blink_cursor);
+        app.add_system(process_cursor_focus);
+        // app.register_element_builder("textinput", build_text_input);
     }
 }
 
 #[derive(Component)]
 pub struct TextInput {
     pub value: String,
-    cursor: Entity
+    cursor: Entity,
+    text: Entity
+}
+
+#[derive(Component, Default)]
+pub struct TextInputCursor {
+    state: f32
 }
 
 
-fn build_text_input(
+widget!( TextInput,
     mut ctx: ResMut<BuildingContext>,
-    mut commands: Commands,
-) {
+    mut commands: Commands
+=> {
     let entity = ctx.element;
-    let t = ctx.attributes.get::<String>("value".as_tag());
-    info!("got text for input 0: {:?}", t);
-    let value = bindattr!(ctx, commands, value:String => TextInput.value);
-    info!("got text for input 1: {:?}", value);
+    let value = bindattr!(ctx, commands, value:String => Self.value);
     let cursor = commands.spawn().id();
-    let block = FocusPolicy::Block;
+    let text = commands.spawn().id();
+    let block_input = FocusPolicy::Block;
     let widget = TextInput {
-        cursor,
+        cursor, text,
         value: value.unwrap_or("".to_string()),
     };
     commands.entity(entity).with_elements(bsx! {
-        <el with=(widget,block,UiColor,Interaction) c:text-input s:background-color="#2f2f2f" s:padding="1px">
-            <el with=UiColor c:text-input-inner 
+        <el with=(widget,block_input,UiColor,Interaction) c:text-input s:background-color="#2f2f2f" s:padding="1px">
+            <el 
+                with=UiColor
+                c:text-input-inner 
                 s:width="100%"
                 s:heigth="100%"
                 s:background-color="#cfcfcf"
@@ -43,12 +51,84 @@ fn build_text_input(
                 s:height="24px"
                 s:width="100%"
             >
-                <el c:text-input-selection/>
-                <text value=bind!(<= entity, TextInput.value) s:color="#2f2f2f"/>
-                <el:cursor c:text-input-cursor/>
+                // <el c:text-input-selection/>
+                <text:text value=bind!(<= entity, Self.value) s:color="#2f2f2f" s:margin-left="2px"/>
+                <el:cursor
+                    with=UiColor
+                    c:text-input-cursor
+                    s:position-type="absolute"
+                    s:top="1px"
+                    s:bottom="1px"
+                    s:left="1px"
+                    // s:right="2px"
+                    s:width="1px"
+                    s:display="none"
+                    // s:height="20px"
+                    s:background-color="#2f2f2f"
+                />
             </el>
         </el>
     });
+});
+
+fn process_keyboard_input(
+    mut inputs: Query<(&mut TextInput, &Element)>,
+    mut cursors: Query<(&mut TextInputCursor, &mut Style)>,
+    mut texts: Query<(&mut Text, &TextLayoutInfo)>,
+
+) {
+    let input = inputs.iter_mut()
+        .filter(|(_, e)| e.focused())
+        .map(|(i, _)| i)
+        .next();
+    let mut input = if let Some(input) = input {
+        input
+    } else {
+        return
+    };
+
+
+
+
+
+}
+
+fn process_cursor_focus(
+    mut commands: Commands,
+    input: Query<(&TextInput, &Element), Changed<Element>>,
+    cursors: Query<&TextInputCursor>,
+    mut styles: Query<&mut Style>,
+) {
+    for (input, element) in input.iter() {
+        if element.focused() && !cursors.contains(input.cursor) {
+            commands.entity(input.cursor).insert(TextInputCursor::default());
+        }
+        if !element.focused() && cursors.contains(input.cursor) {
+            commands.entity(input.cursor).remove::<TextInputCursor>();
+            if let Ok(mut style) = styles.get_mut(input.cursor) {
+                style.display = Display::None;
+            }
+        }
+    }
+}
+
+fn blink_cursor(
+    time: Res<Time>,
+    mut cursor: Query<(&mut TextInputCursor, &mut Style)>,
+) {
+    for (mut cursor, mut style) in cursor.iter_mut() {
+        cursor.state -= time.delta_seconds();
+        if cursor.state < 0. {
+            cursor.state = 1.;
+        }
+        if cursor.state >= 0.5 && style.display == Display::None {
+            style.display = Display::Flex;
+        }
+        if cursor.state < 0.5 && style.display != Display::None {
+            style.display = Display::None;
+        }
+    }
+
 }
 
 fn build_text(
@@ -56,10 +136,7 @@ fn build_text(
     mut commands: Commands,
     default_font: Res<DefaultFont>,
 ) {
-    let t = ctx.attributes.get_variant("value".as_tag());
-    info!("got text 0: {:?}", t);
     let text = bindattr!(ctx, commands, value:String => Text.sections[0].value);
-    info!("got text: {:?}", text);
     commands
         .entity(ctx.element)
         .insert_bundle(TextBundle::from_section(
