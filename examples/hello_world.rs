6ad62ada11496bf74cf6c48a9d71ac368d99c803
Yakov Borevich <j.borevich@gmail.com>
2022-12-07 22:30:19 +0100

    [examples] add hello world example

---
+++ examples/hello_world.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+use bevy_elements::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ElementsPlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.add(eml! {
+        <body s:padding="50px">
+            "Hello, "<strong>"world"</strong>"!"
+        </body>
+    });
+}
