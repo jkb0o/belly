c3842843c7c4adcfb5005b7c5cae778f871ecb7b
Yakov Borevich <j.borevich@gmail.com>
2022-12-22 10:46:00 +0100

    [examples] add predefined colors to color picker example

--- examples/color_picker.rs
+++ examples/color_picker.rs
@@ -9,24 +9,69 @@ fn main() {
         .run();
 }
 
+const COLORS: &[&'static str] = &[
+    // from https://colorswall.com/palette/105557
+    "#f44336", // Red
+    "#e81e63", // Pink
+    "#9c27b0", // Purple
+    "#673ab7", // Deep Purple
+    "#3f51b5", // Indigo
+    "#2196f3", // Blue
+    "#03a9f4", // Light Blue
+    "#00bcd4", // Cyan
+    "#009688", // Teal
+    "#4caf50", // Green
+    "#8bc34a", // Light Green
+    "#cddc39", // Lime
+    "#ffeb3b", // Yellow
+    "#ffc107", // Amber
+    "#ff9800", // Orange
+    "#ff5722", // Deep Orange
+];
+
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
     let colorbox = commands.spawn_empty().id();
     commands.add(eml! {
         <body s:padding="50px">
-            <span c:controls>
-                <slider c:red value=0.75 bind:value=to!(colorbox, BackgroundColor:0|color:r)/>
-                <slider c:green value=1.0 bind:value=to!(colorbox, BackgroundColor:0|color:g)/>
-                <slider c:blue value=1.0 bind:value=to!(colorbox, BackgroundColor:0|color:b)/>
-                <slider c:alpha value=1.0 bind:value=to!(colorbox, BackgroundColor:0|color:a)/>
+            <span c:controls on:ready=connect!(colorbox, |c: BackgroundColor| c.0 = Color::WHITE)>
+                <slider c:red
+                    bind:value=to!(colorbox, BackgroundColor:0|color:r)
+                    bind:value=from!(colorbox, BackgroundColor:0.r())
+                />
+                <slider c:green
+                    bind:value=to!(colorbox, BackgroundColor:0|color:g)
+                    bind:value=from!(colorbox, BackgroundColor:0.g())
+                />
+                <slider c:blue
+                    bind:value=to!(colorbox, BackgroundColor:0|color:b)
+                    bind:value=from!(colorbox, BackgroundColor:0.b())
+                />
+                <slider c:alpha
+                    bind:value=to!(colorbox, BackgroundColor:0|color:a)
+                    bind:value=from!(colorbox, BackgroundColor:0.a())
+                />
             </span>
-            <span {colorbox} c:colorbox/>
+            <img c:colorbox-holder src="trbg.png">
+                <span {colorbox} c:colorbox/>
+            </img>
+            <span c:colors>
+            <for color in = COLORS>
+                <button on:press=connect!(colorbox, |c: BackgroundColor| { c.0 = Color::from_hex(color) })>
+                    <span s:background-color=*color s:width="100%" s:height="100%"/>
+                </button>
+            </for>
+            </span>
         </body>
     });
     commands.add(StyleSheet::parse(
         r#"
+        body {
+            justify-content: center;
+            align-items: center;
+        }
         .controls {
-            width: 350px;
+            width: 200px;
             height: 200px;
             flex-direction: column;
             justify-content: space-around;
@@ -35,9 +80,22 @@ fn setup(mut commands: Commands) {
         .controls slider {
             width: 100%;
         }
-        .colorbox {
+        .colorbox-holder {
             width: 200px;
             height: 200px;
+        }
+        .colorbox {
+            width: 100%;
+            height: 100%;
+        }
+        .colors {
+            flex-wrap: wrap;
+            width: 200px;
+            height: 200px;
+            margin-left: 20px;
+            justify-content: space-between;
+            align-content: space-between;
+            padding: -5px;
         }
         .red .slider-low {
             background-color: #F54C36;
