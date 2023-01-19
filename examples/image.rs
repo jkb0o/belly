61b9aaed9a83cdcf99ccac2c79bb680a97cfd653
Yakov Borevich <j.borevich@gmail.com>
2023-01-19 10:36:20 +0100

    [examples] Fix image example after ess props refactor

--- examples/image.rs
+++ examples/image.rs
@@ -15,7 +15,7 @@ fn setup(mut commands: Commands) {
     commands.add(StyleSheet::parse(
         r#"
         body {
-            wrap: no-wrap;
+            flex-wrap: no-wrap;
             flex-direction: column;
             align-content: center;
             align-items: center;
