2726c4721a37603d1572fd5cf70573eb22877a16
Michel van der Hulst <hulstmichel@gmail.com>
2023-02-17 17:34:55 +0100

    [examples] Add screenshots & descriptions for examples (#39)
    fixes #36

--- examples/counter-binds.rs
+++ examples/counter-binds.rs
@@ -23,7 +23,7 @@ fn setup(mut commands: Commands) {
             // connect the press signal to closure executed on the Counter context
             <button on:press=connect!(counter, |c: Counter| c.count += 1)>"+"</button>
             <span s:width="150px" s:justify-content="center">
-                // bind Counter.count property at counter entity to Label.value proeprty
+                // bind Counter.count property at counter entity to Label.value property
                 <label bind:value=from!(counter, Counter:count|fmt.c("Value: {c}"))/>
             </span>
             <button on:press=connect!(counter, |c: Counter| c.count -= 1)>"-"</button>
